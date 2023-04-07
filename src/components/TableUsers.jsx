df799b210a81761afe9e04ef444ea4ffad9ea082
Thuan0907 <jinkin0019@gmail.com>
2023-04-07 12:07:15 +0700

    read-paginate

--- src/components/TableUsers.jsx
+++ src/components/TableUsers.jsx
@@ -1,5 +1,83 @@
+import { useEffect, useState } from "react";
+import Table from "react-bootstrap/Table";
+import { fetchAllUser } from "../services/UserService";
+import ReactPaginate from "react-paginate";
+
 const TableUsers = () => {
-  return <>Table user</>;
+  const [listUsers, setListUsers] = useState([]);
+  const [totalUsers, setTotalUsers] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
+
+  useEffect(() => {
+    // call API
+    getUsers(1);
+  }, []);
+
+  const getUsers = async (page) => {
+    let res = await fetchAllUser(page);
+
+    if (res && res.data && res.data) {
+      setTotalUsers(res.total);
+      setListUsers(res.data);
+      setTotalPages(res.total_pages);
+    }
+  };
+
+  const handlePageClick = (event) => {
+    getUsers(+event.selected + 1);
+  };
+
+  console.log(">>> listUsers: ", listUsers);
+  return (
+    <>
+      <div className="title">Table users</div>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Email</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {listUsers &&
+            listUsers.length > 0 &&
+            listUsers.map((item, index) => {
+              return (
+                <tr key={`users-${index}`}>
+                  <td>{item.id}</td>
+                  <td>{item.email}</td>
+                  <td>{item.first_name}</td>
+                  <td>{item.last_name}</td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </Table>
+
+      <ReactPaginate
+        breakLabel="..."
+        nextLabel="next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={5}
+        pageCount={totalPages}
+        previousLabel="< previous"
+        renderOnZeroPageCount={null}
+        marginPagesDisplayed={2}
+        pageClassName="page-item"
+        pageLinkClassName="page-link"
+        previousClassName="page-item"
+        previousLinkClassName="page-link"
+        nextClassName="page-item"
+        nextLinkClassName="page-link"
+        breakClassName="page-item"
+        breakLinkClassName="page-link"
+        containerClassName="pagination"
+        activeClassName="active"
+      />
+    </>
+  );
 };
 
 export default TableUsers;
